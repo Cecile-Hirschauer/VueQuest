9a38107197dae55da47653203362709a12af824a
cecile hirschauer <cecile.h.75@gmail.com>
2024-08-14 14:58:09 +0200

    Install vuejs, tailwind. Add Layout.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <template v-slot:main>
+      <div>
+        Main
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+import Layout from "./components/Layout.vue"
+
+</script>
---
+++ src/components/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-col w-full min-h-screen text-white bg-slate-800">
+        <header>
+            <div class="container py-6 mx-auto text-center">
+                <h1 class="text-3xl font-bold text-white">VueQuiz</h1>
+            </div>
+        </header>
+        <main class="container flex-grow p-4 mx-auto">
+            <slot name="main"></slot>
+        </main>
+        <footer class="p-4 text-center bg-teal-700">
+            <p>&copy; 2024 VueQuiz. Tous droits réservés.</p>
+        </footer>
+    </div>
+</template>
