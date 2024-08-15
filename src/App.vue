64096f08990153e351b36450a889f7153842fb8a
cecile hirschauer <cecile.h.75@gmail.com>
2024-08-15 09:45:07 +0200

    add pico css and quiz;json

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,34 @@
   <Layout>
     <template v-slot:main>
       <div>
-        Main
+        {{ quiz }}
       </div>
     </template>
   </Layout>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
 
 import Layout from "./components/Layout.vue"
+
+const quiz = ref(null)
+
+onMounted(() => {
+  fetch("/quiz.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Impossible d'afficher le fichier json");
+      }
+      return response.json()
+    })
+    .then(data => {
+      quiz.value = data
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  console.log(quiz.data)
+})
 
 </script>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,15 +1,68 @@
 <template>
-    <div class="flex flex-col w-full min-h-screen text-white bg-slate-800">
-        <header>
-            <div class="container py-6 mx-auto text-center">
-                <h1 class="text-3xl font-bold text-white">VueQuiz</h1>
-            </div>
+    <div class="wrapper">
+        <!-- Header -->
+        <header class="header">
+            <nav>
+                <ul>
+                    <li>
+                        <h1>VueQuest</h1>
+                    </li>
+                </ul>
+            </nav>
         </header>
-        <main class="container flex-grow p-4 mx-auto">
+
+        <!-- Main Content -->
+        <main class="container">
+            <!-- Slot for main content -->
             <slot name="main"></slot>
         </main>
-        <footer class="p-4 text-center bg-teal-700">
-            <p>&copy; 2024 VueQuiz. Tous droits réservés.</p>
+
+        <!-- Footer -->
+        <footer class="footer">
+            <p class="pico-color-pumpkin-300">&copy; 2024 VueQuest. Tous droits réservés.</p>
         </footer>
     </div>
 </template>
+
+<script setup>
+// Pas de script spécifique pour ce layout
+</script>
+
+<style>
+.wrapper {
+    height: 100vh;
+    display: flex;
+
+    /* Direction of the items, can be row or column */
+    flex-direction: column;
+}
+
+header,
+footer {
+    height: 5%;
+    margin-bottom: 7px;
+}
+
+footer {
+    text-align: center;
+}
+
+main {
+    display: flex;
+    width: 100%;
+    height: 90%;
+    flex-direction: column;
+    margin-top: 40px;
+    align-items: center;
+    justify-content: center;
+}
+
+h1 {
+    font-size: 60px;
+    font-weight: 600;
+    background: -webkit-linear-gradient(#FFBF00, #FFA23A, #D24317);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    margin-left: 20px;
+}
+</style>
